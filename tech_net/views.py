827efa68d8d7f03c8da267d872921c965ea5c936
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-12-30 18:48:55 +0300

    boop

--- tech_net/views.py
+++ tech_net/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from users.permissions import IsActiveStaff
+from .models import Entity
+from .serializers import EntitySerializer
 
-# Create your views here.
+class EntityViewSet(viewsets.ModelViewSet):
+    queryset = Entity.objects.all()
+    serializer_class = EntitySerializer
+    permission_classes = [IsActiveStaff]  # Ограничение доступа только для авторизованных пользователей
+    filter_backends = [filters.SearchFilter]  # Подключение фильтров
+    search_fields = ['country']  # Добавление фильтрации по стране
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        # Поле "debt" игнорируется даже если передано
+        serializer.save(debt=self.get_object().debt)
