653dffa276b7f3d8eb2a6af70799483e3e852e35
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-12-30 16:09:57 +0300

    created and applied migrations, created csu command, added admin functionality

--- tech_net/admin.py
+++ tech_net/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from tech_net.models import Entity, Product
+
+
+@admin.action(description="Очистить задолженность перед поставщиком")
+def clear_debt(self, request, queryset):
+    updated = queryset.update(debt=0.00)
+    self.message_user(request, f"Задолженность успешно очищена у {updated} объектов.")
+
+@admin.register(Entity)
+class EntityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'entity_type', 'city', 'supplier_link', 'debt', 'created_at', 'hierarchy_level')
+    list_filter = ('city',)
+    actions = [clear_debt]
+
+    def supplier_link(self, obj):
+        if obj.supplier:
+            url = reverse('admin:tech_net_entity_change', args=[obj.supplier.id])
+            return format_html('<a href="{}">{}</a>', url, obj.supplier.name)
+        return "Нет поставщика"
+    supplier_link.short_description = "Поставщик"
+
+
+@admin.register(Product)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'release_date', 'organization')
---
+++ users/management/commands/csu.py
@@ -0,0 +1,14 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Создаёт админа"""
+    def handle(self, *args, **options):
+        user = User.objects.create(username='admin')
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.set_password('test')
+        user.save()
